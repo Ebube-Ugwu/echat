2b065fae9c4f9b88cea8f5a1c9faea04a75d1f26
Ebube Ugwu <137618036+Ebube-Ugwu@users.noreply.github.com>
2026-06-26 16:49:09 +0100

    chore: init spring

---
+++ echat-backend/src/main/java/com/example/echat_backend/EchatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.echat_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchatBackendApplication.class, args);
+	}
+
+}
